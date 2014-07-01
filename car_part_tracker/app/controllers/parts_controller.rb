323c4618241f57ed408b5076ae8fbe2ee2fa6477
Ben Karl <benrkarl@gmail.com>
2014-06-30 23:28:11 -0400

    add root route and empty controllers for cars and parts

---
+++ car_part_tracker/app/controllers/parts_controller.rb
@@ -0,0 +1,12 @@
+class PartsController < ApplicationController
+
+  # parts GET    /parts(.:format)          parts#index
+  #         POST   /parts(.:format)          parts#create
+  # new_part GET    /parts/new(.:format)      parts#new
+  # edit_part GET    /parts/:id/edit(.:format) parts#edit
+  #    part GET    /parts/:id(.:format)      parts#show
+  #         PATCH  /parts/:id(.:format)      parts#update
+  #         PUT    /parts/:id(.:format)      parts#update
+  #         DELETE /parts/:id(.:format)      parts#destroy
+
+end
--- car_part_tracker/config/routes.rb
+++ car_part_tracker/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/' => 'cars#index'
+
   resources :cars
   resources :parts
 end
